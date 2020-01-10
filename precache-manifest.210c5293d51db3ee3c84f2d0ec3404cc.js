df39bb2e2897fe0f5b5b8eb408c5c18df72f695d
Mariam Harutyunyan <marharutyun@gmail.com>
2020-01-10 12:21:34 +0400

    Updates

---
+++ precache-manifest.210c5293d51db3ee3c84f2d0ec3404cc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc89b5219022bf408c9c03ca799665b",
+    "url": "/room-reservation/index.html"
+  },
+  {
+    "revision": "f05801add65b7db828eb",
+    "url": "/room-reservation/static/css/2.ee7a674d.chunk.css"
+  },
+  {
+    "revision": "cd75aef10ea0f1e201da",
+    "url": "/room-reservation/static/css/main.9478636a.chunk.css"
+  },
+  {
+    "revision": "f05801add65b7db828eb",
+    "url": "/room-reservation/static/js/2.470c7e62.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/room-reservation/static/js/2.470c7e62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd75aef10ea0f1e201da",
+    "url": "/room-reservation/static/js/main.f24d8f52.chunk.js"
+  },
+  {
+    "revision": "196098948edbb241b0d8",
+    "url": "/room-reservation/static/js/runtime-main.c88bb33b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/room-reservation/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/room-reservation/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/room-reservation/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/room-reservation/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/room-reservation/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
